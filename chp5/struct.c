d20e3f921fc9e64b4e996c9023c25dc414bb9a4c
codythegreat <maxiecody@gmail.com>
2019-10-21 22:15:44 -0500

    started work on chapter 5

---
+++ chp5/struct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// note that if we add an extra value here later on
+// we shouldn't need to change functions/parameters
+// I'm on page 267
+struct fish { 
+    const char *name;
+    const char *species;
+    int teeth;
+    int age;
+};
+
+struct fish goldy = {"Goldy", "Gold Fish", 32, 2};
+
+void printFish(struct fish f)
+{
+    printf("%s is a %s that has %i teeth and is %i years old.\n",
+     f.name, f.species, f.teeth, f.age);
+}
+
+int main()
+{
+    printFish(goldy);
+}
